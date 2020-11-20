18f6bf2efd64072fb7dee0f2d169f577a079af7f
lafeillou <lafeillou@126.com>
2020-11-20 12:21:43 +0800

    第9次作业

--- lesson9/src/index.js
+++ lesson9/src/index.js
@@ -1,7 +1,8 @@
 // import React, {Component, useState} from "react";
-// import ReactDOM from "react-dom";
-import ReactDOM, {useState} from "./kreact/react-dom";
-import Component from "./kreact/Component";
+import ReactDOM from "react-dom";
+// import ReactDOM, {useState} from "./kreact/react-dom";
+// import Component from "./kreact/Component";
+import React, {Component, useCallback, useMemo,  PureComponent, useState} from "react";
 
 import "./index.css";
 
@@ -31,12 +32,95 @@ function FunctionComponent(props) {
   );
 }
 
+// key01 第9次作业，暗号： 椰子
+function UseCallbackPage(props) {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState("");
+
+  // 不缓存时候的对比
+  // const addClick = () => {
+  //   let sum = 0;
+  //   for(let i = 0; i < count; i++) {
+  //     sum += i;
+  //   }
+  //   return sum;
+  // }
+  
+  const addClick = useCallback( () => {
+    let sum = 0;
+    for(let i = 0; i < count; i++) {
+      sum += i;
+    }
+    return sum;
+  }, [value]) // 依赖可以用改成 value, 观察与count时的不同
+
+
+
+  return (
+    <div className="border">
+      <h3>UseCallbackPage</h3>
+      <p>{count}</p>
+      <button onClick={ () => {setCount(count + 1)}}>add</button>
+      <input value={value}  onChange={event => setValue(event.target.value)} />
+      <Child addClick = {addClick} />
+    </div>
+  )
+}
+
+class Child extends PureComponent {
+  render() {
+    console.log("child render");
+    const {addClick} = this.props;
+    return (
+      <div>
+        <h3>Child</h3>
+        <button onClick={ () => console.log(addClick())}>add</button>
+      </div>
+    )
+  }
+}
+
+// key02 第9次作业，暗号： 椰子
+function UseMemoPage(props) {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState("");
+  // 不缓存时候的对比
+  // const expensive = () => {
+  //   console.log("compute");
+  //   let sum = 0;
+  //   for (let i = 0; i < count; i++) {
+  //   sum += i;
+  //   }
+  //   return sum;
+  // };
+
+  const expensive = useMemo( () => {
+    console.log("compute");
+    let sum = 0;
+    for (let i = 0; i < count; i++) {
+    sum += i;
+    }
+    return sum;
+  }, [value]) // 依赖可以用改成 value, 观察与count时的不同
+  return (
+  <div className="border">
+    <h3>UseMemoPage</h3>
+    <p>expensive:{expensive}</p>
+    <p>{count}</p>
+    <button onClick={() => setCount(count + 1)}>add</button>
+    <input value={value} onChange={event => setValue(event.target.value)} />
+    </div>
+  );
+}
+
 const jsx = (
   <div className="border">
     <p>全栈</p>
     <a href="https://www.kaikeba.com/">开课吧</a>
-    <FunctionComponent name="函数组件" />
-    <ClassComponent name="类组件" />
+    {/* <FunctionComponent name="函数组件" />
+    <ClassComponent name="类组件" /> */}
+    <UseCallbackPage />
+    <UseMemoPage />
   </div>
 );
 
